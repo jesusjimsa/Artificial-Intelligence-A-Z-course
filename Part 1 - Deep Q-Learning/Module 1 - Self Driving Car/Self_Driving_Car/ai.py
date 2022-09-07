418fe6ba787f4d60e691319064f7f6d37b8b00b3
jesusjimsa <jesusjimsa@icloud.com>
2022-09-07 16:18:00 +0100

    ReplayMemory class

--- Part 1 - Deep Q-Learning/Module 1 - Self Driving Car/Self_Driving_Car/ai.py
+++ Part 1 - Deep Q-Learning/Module 1 - Self Driving Car/Self_Driving_Car/ai.py
@@ -21,3 +21,29 @@ class Network(nn.Module):
         self.nb_action = nb_action
         self.fc1 = nn.Linear(input_size, 30)
         self.fc2 = nn.Linear(30, nb_action)
+
+    def forward(self, state):
+        x = F.relu(self.fc1(state))    # Hidden neurons
+        q_values = self.fc2(x)
+
+        return q_values
+
+
+# Implementing Expirience Replay
+class ReplayMemory(object):
+
+    def __init__(self, capacity):
+        self.capacity = capacity    # Maximum number of transitions in our memory
+        self.memory = []
+
+    def push(self, event):
+        self.memory.append(event)
+
+        # If memory exceeds capacity after adding the new event, we'll delete the oldest event in the list
+        if len(self.memory) > self.capacity:
+            del self.memory[0]
+
+    def sample(self, batch_size):
+        samples = zip(*random.sample(self.memory, batch_size))
+
+        return map(lambda x: Variable(torch.cat(x, 0)), samples)
