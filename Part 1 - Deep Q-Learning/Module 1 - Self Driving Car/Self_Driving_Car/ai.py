f5e1c93059fcc87be6021f8727fea0c79f4b7ac0
jesusjimsa <jesusjimsa@icloud.com>
2022-09-07 16:45:17 +0100

    Dqn class: __init__ method

--- Part 1 - Deep Q-Learning/Module 1 - Self Driving Car/Self_Driving_Car/ai.py
+++ Part 1 - Deep Q-Learning/Module 1 - Self Driving Car/Self_Driving_Car/ai.py
@@ -47,3 +47,17 @@ class ReplayMemory(object):
         samples = zip(*random.sample(self.memory, batch_size))
 
         return map(lambda x: Variable(torch.cat(x, 0)), samples)
+
+
+# Implementing Deep Q Learning
+class Dqn():
+
+    def __init__(self, input_size, nb_action, gamma):
+        self.gamma = gamma
+        self.reward_window = []
+        self.model = Network(input_size, nb_action)
+        self.memory = ReplayMemory(100000)
+        self.optimizer = optim.Adam(self.model.parameters(), lr=0.001)
+        self.last_state = torch.Tensor(input_size).unsqueeze(0)
+        self.last_action = 0
+        self.last_reward = 0
