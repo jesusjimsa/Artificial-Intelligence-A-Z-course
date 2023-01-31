df9198addcf10fdf4621d7086fb2c0094de1aa42
jesusjimsa <jesusjimsa@icloud.com>
2023-01-31 19:41:06 +0000

    Dqn class: learn method

--- Part 1 - Deep Q-Learning/Module 1 - Self Driving Car/Self_Driving_Car/ai.py
+++ Part 1 - Deep Q-Learning/Module 1 - Self Driving Car/Self_Driving_Car/ai.py
@@ -67,3 +67,13 @@ class Dqn():
         action = probs.multinomial()
 
         return action.data[0, 0]
+
+    def learn(self, batch_state, batch_next_state, batch_reward, batch_action):
+        outputs = self.model(batch_state).gather(1, batch_action.unsqueeze(1)).squeeze(1)
+        next_outputs = self.model(batch_next_state).detach().max(1)[0]
+        target = (self.gamma * next_outputs) + batch_reward
+        td_loss = F.smooth_l1_loss(outputs, target)  # td = temporal difference
+
+        self.optimizer.zero_grad()
+        td_loss.backward(retain_variables=True)
+        self.optimizer.step()
