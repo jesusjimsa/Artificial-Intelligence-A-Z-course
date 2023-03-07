3f4bdc5d1154e8c84e8411a1c7a1eeef9582d156
jesusjimsa <jesusjimsa@icloud.com>
2023-03-07 20:25:59 +0000

    Training the AI with Deep Convolutional Q-Learning

--- Part 2 - Deep Convolutional Q-Learning/Module 2 - Doom/ai.py
+++ Part 2 - Deep Convolutional Q-Learning/Module 2 - Doom/ai.py
@@ -10,6 +10,7 @@ from torch.autograd import Variable
 
 # Importing the packages for OpenAI and Doom
 import gym
+# The functions needed from these modules are now deprecated, this code will NOT work
 from gym.wrappers import SkipWrapper
 from ppaquette_gym_doom.wrappers.action_space import ToDiscrete
 
@@ -84,3 +85,92 @@ class AI:
         return actions.data.numpy()
 
 # Part 2 - Training the AI with Deep Convolutional Q-Learning
+
+
+# Getting the Doom environment
+
+doom_env = image_preprocessing.PreprocessImage(SkipWrapper(4)(ToDiscrete("minimal")(
+    gym.make("ppaquette/DoomCorridor-v0"))), width=80, height=80, grayscale=True)
+doom_env = gym.wrappers.Monitor(doom_env, "videos", force=True)
+number_actions = doom_env.action_space.n
+
+
+# Building an AI
+
+cnn = CNN(number_actions)
+softmax_body = SoftmaxBody(Temperature=1)
+ai = AI(brain=cnn, body=softmax_body)
+
+
+# Setting up Experience Replay
+
+n_steps = experience_replay.NStepProgress(env=doom_env, ai=ai, n_step=10)
+memory = experience_replay.ReplayMemory(n_steps=n_steps, capacity=10000)
+
+
+# Implemmenting Eligibility Trace
+def eligibility_trace(batch):
+    gamma = 0.99
+    inputs = []
+    targets = []
+
+    for series in batch:
+        input = Variable(torch.from_numpy(np.array([series[0].state, series[-1].state], dtype=np.float32)))
+        output = cnn(input)
+        cumul_reward = 0.0 if series[-1].done else output[1].data.max()
+
+        for step in reversed(series[:-1]):
+            cumul_reward = step.reward + gamma + cumul_reward
+
+        state = series[0].state
+        target = output[0].data
+        target[series[0].action] = cumul_reward
+        inputs.append(state)
+        targets.append(target)
+
+    return torch.from_numpy(np.array(inputs, dtype=np.float32)), torch.stack(targets)
+
+
+# Making the moving average on 100 steps
+class MA:   # Moving Average
+    def __init__(self, size):
+        self.list_of_rewards = []
+        self.size = size
+
+    def add(self, rewards):
+        if isinstance(rewards, list):
+            self.list_of_rewards += rewards
+        else:
+            self.list_of_rewards.append(rewards)
+
+        while len(self.list_of_rewards) > self.size:
+            del self.list_of_rewards[0]
+
+    def average(self):
+        return np.mean(self.list_of_rewards)
+
+
+ma = MA(100)
+
+# Training the AI
+loss = nn.MSELoss()
+optimizer = optim.Adam(cnn.parameters(), lr=0.001)
+nb_epochs = 100
+
+for epoch in range(1, nb_epochs + 1):
+    memory.run_steps(200)
+
+    for batch in memory.sample_batch(128):
+        inputs, targets = eligibility_trace(batch)
+        inputs, targets = Variable(inputs), Variable(targets)
+        predictions = cnn(inputs)
+        loss_error = loss(predictions, targets)
+        optimizer.zero_grad()
+        loss_error.backward()
+        optimizer.step()
+
+    rewards_steps = n_steps.rewards_steps()
+    ma.add(rewards_steps)
+    avg_reward = ma.average()
+
+    print("Epoch %s, Average Reward: %s" % (str(epoch), str(avg_reward)))
