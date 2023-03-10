de3ad5d0b13970f66cc84537ba930eaca76e079d
jesusjimsa <jesusjimsa@icloud.com>
2023-03-10 15:45:57 +0000

    Initializing and setting the variance of a tensor of weights

--- Part 3 - A3C/Module 3 - Breakout/model.py
+++ Part 3 - A3C/Module 3 - Breakout/model.py
@@ -9,7 +9,11 @@ import torch.nn.functional as F
 
 # Initializing and setting the variance of a tensor of weights
 
+def normalized_columns_initializer(weights, std=1.0):
+    out = torch.randn(weights.size())
+    out *= std / torch.sqrt(out.pow(2).sum(1).expand_as(out))   # var(out) = std^2
 
+    return out
 
 # Initializing the weights of the neural network in an optimal way for the learning
 
