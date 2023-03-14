2b170c33a54c48ca868dfc737cb30b561391e6c7
jesusjimsa <jesusjimsa@icloud.com>
2023-03-14 20:47:28 +0000

    Training function

--- Part 3 - A3C/Module 3 - Breakout/train.py
+++ Part 3 - A3C/Module 3 - Breakout/train.py
@@ -13,3 +13,82 @@ def ensure_shared_grads(model, shared_model):
             return
 
         shared_param._grad = param.grad
+
+
+def train(rank, params, shared_model, optimizer):
+    torch.manual_seed(params.seed + rank)
+    env = create_atari_env(params.env_name)
+    env.seed(params.seed + rank)
+    model = ActorCritic(env.observation_space.shape[0], env.action_space)
+    state = env.reset()
+    state = torch.from_numpy(state)
+    done = True
+    episode_length = 0
+
+    while True:
+        episode_length += 1
+        model.load_state_dict(shared_model.state_dict())
+
+        if done:
+            cx = Variable(torch.zero(1, 256))
+            hx = Variable(torch.zero(1, 256))
+        else:
+            cx = Variable(cx.data)
+            hx = Variable(hx.data)
+
+        values = []
+        log_probs = []
+        rewards = []
+        entropies = []
+
+        for _ in range(params.num_steps):
+            value, action_values, (hx, cx) = model((Variable(state.unsqueeze(0)), (hx, cx)))
+            prob = F.softmax(action_values)
+            log_prob = F.log_softmax(action_values)
+            entropy = -(log_prob * prob).sum(1)
+            action = prob.multinomial().data
+            log_prob = log_prob.gather(1, Variable(action))
+            state, reward, done = env.step(action.numpy())
+            done = (done or episode_length > params.max_episode_length)
+            reward = max(min(reward, 1), -1)
+
+            if done:
+                episode_length = 0
+                state = env.reset()
+
+            state = torch.from_numpy(state)
+
+            entropies.append(entropy)
+            values.append(value)
+            log_probs.append(log_prob)
+            rewards.append(reward)
+
+            if done:
+                break
+
+        R = torch.zeros(1, 1)
+
+        if not done:
+            value, _, _ = model((Variable(state.unsqueeze(0)), (hx, cx)))
+            R = value.data
+
+        values.append(Variable(R))
+
+        policy_loss = 0
+        value_loss = 0
+        R = Variable(R)
+        gae = torch.zeros(1, 1)     # A(a, s) = Q(a, s) - V(s)
+
+        for i in reversed(range(len(rewards))):
+            R = params.gamma * R + rewards[i]
+            advantage = R - values[i]
+            value_loss += (0.5 * advantage.pow(2))  # Q(a*, s) = V*(s)
+            TD = rewards[i] + params.gamma * values[i + 1].data - values[i].data
+            gae = gae * params.gamma * params.tau + TD
+            policy_loss = policy_loss - log_probs[i] * Variable(gae) - 0.01 * entropies[i]
+
+        optimizer.zero_grad()
+        (policy_loss + 0.5 * value_loss).backward()
+        torch.nn.utils.clip_grad_norm(model.parameters(), 40)
+        ensure_shared_grads(model, shared_model)
+        optimizer.step()
