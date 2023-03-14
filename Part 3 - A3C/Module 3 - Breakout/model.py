63a61ee1179fb8663290b084ad3de06a887c66c6
jesusjimsa <jesusjimsa@icloud.com>
2023-03-14 19:43:42 +0000

    Making the A3C brain

--- Part 3 - A3C/Module 3 - Breakout/model.py
+++ Part 3 - A3C/Module 3 - Breakout/model.py
@@ -38,3 +38,35 @@ def weights_init(m):
 
 # Making the A3C brain
 
+class ActorCritic(torch.nn.Module):
+
+    def __init__(self, num_inputs, action_space):
+        super(ActorCritic, self).__init__
+        self.conv1 = nn.Conv2d(num_inputs, 32, 3, stride=2, padding=1)
+        self.conv2 = nn.Conv2d(32, 32, 3, stride=2, padding=1)
+        self.conv3 = nn.Conv2d(32, 32, 3, stride=2, padding=1)
+        self.conv4 = nn.Conv2d(32, 32, 3, stride=2, padding=1)
+        self.lstm = nn.LSTMCell(32 * 3 * 3, 256)
+        num_outputs = action_space.n
+        self.critic_linear = nn.Linear(256, 1)  # output = V(S)
+        self.actor_linear = nn.Linear(256, num_outputs)     # output = Q(S, A)
+        self.apply(weights_init)
+        self.actor_linear.weight.data = normalized_columns_initializer(self.actor_linear.weight.data, 0.01)
+        self.actor_linear.bias.data.fill(0)
+        self.critic_linear.weight.data = normalized_columns_initializer(self.critic_linear.weight.data, 0.01)
+        self.critic_linear.bias.data.fill(0)
+        self.lstm.bias_ih.data.fill_(0)
+        self.lstm.bias_hh.data.fill_(0)
+        self.train()
+
+    def forward(self, inputs):
+        inputs, (hx, cx) = inputs
+        x = F.elu(self.conv1(inputs))
+        x = F.elu(self.conv2(x))
+        x = F.elu(self.conv3(x))
+        x = F.elu(self.conv4(x))
+        x = x.view(-1, 32 * 3 * 3)
+        (hx, cx) = self.lstm(x, (hx, cx))
+        x = hx
+
+        return self.critic_linear(x), self.actor_linear(x), (hx, cx)
